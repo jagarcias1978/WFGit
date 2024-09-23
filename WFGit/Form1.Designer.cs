247662c5cc282c3ef2a0062d4a3c1c0183455123
Jose Alberto Garcia <jagarcias1978@gmail.com>
2024-09-23 14:25:39 +0200

    Cambio de título

--- WFGit/Form1.Designer.cs
+++ WFGit/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1205, 722);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Formulario de ejemplo";
             this.ResumeLayout(false);
 
         }
