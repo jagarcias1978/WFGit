e75633cd0f72f2ffe06bdc7348fe8039e8d14350
Jose Alberto Garcia <jagarcias1978@gmail.com>
2024-09-24 10:26:54 +0200

    Se añade un segndo botón

--- WFGit/Form1.Designer.cs
+++ WFGit/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSegundo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAceptar
@@ -40,11 +41,21 @@
             this.btnAceptar.Text = "ACEPTAR";
             this.btnAceptar.UseVisualStyleBackColor = true;
             // 
+            // btnSegundo
+            // 
+            this.btnSegundo.Location = new System.Drawing.Point(530, 382);
+            this.btnSegundo.Name = "btnSegundo";
+            this.btnSegundo.Size = new System.Drawing.Size(212, 112);
+            this.btnSegundo.TabIndex = 1;
+            this.btnSegundo.Text = "SEGUNDO";
+            this.btnSegundo.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1205, 722);
+            this.Controls.Add(this.btnSegundo);
             this.Controls.Add(this.btnAceptar);
             this.Name = "Form1";
             this.Text = "Formulario de ejemplo modificado";
@@ -55,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnSegundo;
     }
 }
 
